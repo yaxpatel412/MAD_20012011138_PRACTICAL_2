d04e7c7a40bd8970b53d43cc38eb487ea4903819
Yaxpatel123 <87279091+Yaxpatel123@users.noreply.github.com>
2022-08-28 23:44:59 +0530

    PRACTICAL_2

---
+++ app/src/main/java/com/example/android/mad_20012011138_practical_2/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.android.a20012011138_practical_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMessage("onCreate is  called")
+    }
+
+    private fun showMessage(msg:String)
+    {
+        Log.i(TAG,msg)
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
+        Snackbar.make(findViewById(R.id.CL1),msg,Snackbar.LENGTH_LONG).show()
+    }
+    override fun onStart()
+    {
+        super.onStart()
+        showMessage("onStart is called")
+    }
+    override fun onResume()
+    {
+        super.onResume()
+        showMessage("onResume is called")
+    }
+    override fun onPause()
+    {
+        super.onPause()
+        showMessage("onPause is called")
+    }
+    override fun onStop()
+    {
+        super.onStop()
+        showMessage("onStop is called")
+    }
+
+    override fun onDestroy()
+    {
+        super.onDestroy()
+        showMessage("onDestroy is called")
+    }
+}
